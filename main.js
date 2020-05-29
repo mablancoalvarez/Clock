8cf9decfb17252cef404e7cd70ac8842f760aee3
mablancoalvarez <mablancoalvarez@gmail.com>
2020-05-29 13:54:18 +0200

    fix minuts

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
 const now = new Date ();
+
 const seconds = now.getSeconds();
 const secondsDegrees = ((seconds / 60) * 360) + 90;
 secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+if (seconds === 60){
+    secondHand.style.transform = 'none';
+}else {
+    secondHand.style.transition = 'all 0.05s';
+}
+
+
 
 const mins = now.getMinutes();
 const minsDegrees = ((mins / 60) * 360) + 90;
@@ -17,7 +25,7 @@ minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 const hours = now.getHours();
 const hoursDegrees = ((hours / 12) * 360) + 90;
 hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
+console.log(seconds)
 }
 
 setInterval(setDate, 1000);
